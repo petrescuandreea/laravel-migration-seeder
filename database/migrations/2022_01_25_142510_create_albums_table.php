4cc916cc88579f8dce448e9795798fa9da9db1e2
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-25 16:00:45 +0100

    Create albums migration

---
+++ database/migrations/2022_01_25_142510_create_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+
+            $table -> id();
+
+            $table -> string('title');
+            $table -> date('date_of_release');
+            $table -> tinyInteger('tracks');
+            $table -> string('produced_by');
+            $table -> tinyInteger('n_discs');
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
