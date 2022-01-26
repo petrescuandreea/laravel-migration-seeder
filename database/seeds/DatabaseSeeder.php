a8d206e9a9e5ca95db5a820da2bdd4a9d539c791
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-26 15:04:42 +0100

    Add seeders in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            SongSeeder::class,
+            ArtistSeeder::class,
+            AlbumSeeder::class
+        ]);
     }
 }
