42f80e376db2d6e8b89eebddb7aa8cbd485dc40a
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-27 10:00:13 +0100

    Update album factory

--- database/factories/AlbumFactory.php
+++ database/factories/AlbumFactory.php
@@ -7,11 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Album::class, function (Faker $faker) {
     return [
-        'title' => $faker -> name(60),
+        'title' => $faker -> words(3, true),
         'date_of_release' => $faker -> date(),
         'produced_by' => $faker -> name(60),
-        'views'=> $faker -> numberBetween(),
-        'sales' => $faker -> numberBetween(),
+        'views'=> $faker -> randomNumber(3),
+        'sales' => $faker -> randomNumber(3),
         'cover' => $faker -> imageUrl(360, 360, 'animals', true, 'dogs', true)
     ];
 });
